9ceab95fe60f784e06ff3b08fe3558003ebd2e56
Chaeyeon <61906343+chyyyn@users.noreply.github.com>
2025-05-28 07:49:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+(function () {
+    const spanEl = document.querySelector('main h2 span');
+    const txtArr = ['Front-End Developer', 'Creative Coder', 'Problem Solver', 'UI/UX Enthusiast', 'Web Innovator'];
+
+    let index = 0;
+    let currentTxt = txtArr[index].split("");
+
+    function writeTxt() {
+        spanEl.textContent += currentTxt.shift();
+        if (currentTxt.length != 0) {
+            setTimeout(writeTxt, Math.floor(Math.random() * 105));
+        } else {
+            currentTxt = spanEl.textContent.split("");
+            setTimeout(deleteTxt, 3000);
+        }
+    }
+
+    function deleteTxt() {
+        currentTxt.pop();
+        spanEl.textContent = currentTxt.join("");
+        if (currentTxt != 0) {
+            setTimeout(deleteTxt, Math.floor(Math.random() * 105))
+        } else {
+            index = (index + 1) % txtArr.length;
+            currentTxt = txtArr[index].split("");
+            writeTxt();
+        }
+    }
+
+    writeTxt()
+})();
+
+// 스크롤하면 header태그에 active클래스 추가 및 삭제
+(function () {
+    const headerEl = document.querySelector('header');
+    window.addEventListener("scroll", function () {
+        this.requestAnimationFrame(scrollCheck);
+    })
+
+    function scrollCheck() {
+        const browserScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+        if (browserScrollY > 0) {
+            headerEl.classList.add('active');
+        } else {
+            headerEl.classList.remove('active');
+        }
+    }
+})();
+
+const animationMove = (selector) => {
+    const target = document.querySelector(selector);
+    const browserScrollY = window.pageYOffset;
+    const targetScrollY = target.getBoundingClientRect().top + browserScrollY;
+    window.scrollTo({top: targetScrollY, behavior: 'smooth'})
+    console.log(target);
+}
+
+const scrollMoveEl = document.querySelectorAll("[data-animation-scroll='true']");
+for (let i = 0; i < scrollMoveEl.length; i++) {
+    scrollMoveEl[i].addEventListener('click', function (e) {
+        animationMove(e.target.dataset.target);
+    })
+}
